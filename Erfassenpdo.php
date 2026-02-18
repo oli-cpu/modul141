6ec765990a4c5006a068657deab8fbf0d4bb6582
Oliver Studer <oliver.studer@bbz-sh.ch>
2026-02-18 12:58:55 +0100

    nga

---
+++ Erfassenpdo.php
@@ -0,0 +1,14 @@
+<form method="post" action="M141ErfassenGOpdo.php">  
+<table border="0" cellpadding="0" cellspacing="0" width="451"> 
+ <table border="1" bordercolorlight="#000000" bordercolor="#000000" cellspacing="0">
+  <tr>
+    <td><b>Artikelname</b></td>
+    <td><b>Preis</b></td>
+  </tr>
+  <tr> 
+    <td><input name="Artname" type="text"></td>
+    <td><input name="Artpreis" type="text"></td>   
+
+  </tr>
+        </table> 
+<input type="submit" name="undGoButton" value="Artikel erfassen"> 
